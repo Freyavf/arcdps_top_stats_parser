1efa8d9fe0d52b584b418a0e1f070fb6fb335b82
Freya Fleckenstein <fleckenf@informatik.uni-freiburg.de>
2022-03-07 15:23:39 +0100

    rm sql config

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ class Config:
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "C7baf5kXsK7ahGnYYCQ7L6BQXZ9RyL9vENJ8")
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 
 class ProductionConfig(Config):
