b0ac9b21329be3a38526602da424762614f84d30
kvssr <kevin.visser.info@gmail.com>
2022-03-16 13:32:13 +0100

    Stripping project

--- parsing_arc_top_stats.bat
+++
@@ -1,3 +0,0 @@
-for %%i in (%1\*.zevtc) do (%2\GuildWars2EliteInsights.exe -c %3\EI_config\EI_detailed_json_combat_replay.conf "%%i")
-python %3/parse_top_stats_overview.py %1
-python %3/parse_top_stats_detailed.py %1
