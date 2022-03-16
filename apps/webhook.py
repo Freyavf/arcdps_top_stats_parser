e844409bf1444ec99158bcfd66d7249a6422f880
kvssr <kevin.visser.info@gmail.com>
2022-03-16 12:50:42 +0100

    Increased Threads

--- apps/webhook.py
+++ apps/webhook.py
@@ -4,7 +4,8 @@ import json
 # Test function to send a request with links to the json page
 # You can run this file to send the request
 def get_raid_json():
-    url = 'http://127.0.0.1:5000/json'
+    #url = 'http://127.0.0.1:5000/json'
+    url = 'https://arc-parser-api.herokuapp.com/json'
 
     data = {'links':[
         {'href': 'https://dps.report/getJson?id=y0LO-20220127-203541'},
